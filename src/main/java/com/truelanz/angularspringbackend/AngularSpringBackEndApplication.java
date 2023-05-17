ec7966e17717cc6da071aff0dbd7e30112380c18
truelanz <devalanoliveira@gmail.com>
2023-05-17 15:36:28 -0300

    first commit

---
+++ src/main/java/com/truelanz/angularspringbackend/AngularSpringBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.truelanz.angularspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBackEndApplication.class, args);
+	}
+
+}
